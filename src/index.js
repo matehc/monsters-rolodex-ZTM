4f378328b1665fb7913d13251c704d64c8f6956c
matehc <lucas.matehc@gmail.com>
2022-08-17 06:43:05 +0100

    updated to React 18

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// from React 18, we import createRoot from 'react-dom/client'
+
+// we can capture the root id from html and store it in a container.
+const constainer = document.getElementById('root');
+
+// pass captured root element into createRoot that will become our new root.
+const root = createRoot(constainer);
+
+// call the render method on the root that has been created
+root.render (
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
